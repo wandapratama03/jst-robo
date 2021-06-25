f57a2201db62a0c5370ed71cb066ef0b718b7865
wandapratama03 <84654036+wandapratama03@users.noreply.github.com>
2021-06-25 11:09:48 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/anzala0807/week-15-/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/wandapratama03/jst-robo/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
